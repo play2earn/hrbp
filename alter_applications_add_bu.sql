833df0c5c4fd1be6250d2b60ea3735e6494eb5f2
Copper Hodl <delta@Coppers-MacBook-Pro.local>
2026-05-21 18:18:20 +0700

    build(git): remove existing .sql and .md files from git tracking (keep locally)

--- alter_applications_add_bu.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE applications ADD COLUMN IF NOT EXISTS business_unit TEXT;
--- alter_applications_policy_auth.sql
+++
@@ -1,5 +0,0 @@
--- Policy: Allow authenticated users to insert applications
-create policy "Enable insert for authenticated users"
-on applications for insert
-to authenticated
-with check (true);
